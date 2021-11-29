55033ac583a4208b0ac21216a32f47bd13db5785
JT Raber <JMANN240raber@gmail.com>
2021-11-28 19:06:08 -0500

    nearly there

--- burgertime.py
+++ burgertime.py
@@ -1,6 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+from flask.json import JSONEncoder
+import mysql.connector
+import datetime
+from time import time
+
+class CustomJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        try:
+            if isinstance(obj, datetime.datetime):
+                return obj.strftime("%x %X %z")
+            iterable = iter(obj)
+        except TypeError:
+            pass
+        else:
+            return list(iterable)
+        return JSONEncoder.default(self, obj)
 
 app = Flask(__name__)
+app.json_encoder = CustomJSONEncoder
 
 @app.route("/")
 def index():
@@ -8,11 +25,84 @@ def index():
 
 @app.route("/new_order")
 def new_order():
-    return "new order page"
+    return render_template("order.html")
 
 @app.route("/employee_options")
 def employee_options():
-    return "employee options page"
+    return render_template("employee.html")
 
+@app.route("/api/clock", methods=["POST"])
+def api_clock():
+    res_json = request.json
+    connection = mysql.connector.connect(host="burgertime.ejrobotics.com", database="burgertime", user="jt", password="Bungertime1")
+    if connection.is_connected():
+        cursor = connection.cursor(dictionary=True)
+        vals = (res_json["employeeID"], res_json["clockType"])
+        cursor.execute("INSERT INTO timeclock VALUES (%s, NOW(), %s);", vals)
+        connection.commit()
+        cursor.execute("SELECT MAX(orderID) AS orderID FROM customerOrder;")
+        row = cursor.fetchone()
+    return jsonify(row)
+
+@app.route("/api/dishes")
+def api_dishes():
+    connection = mysql.connector.connect(host="burgertime.ejrobotics.com", database="burgertime", user="jt", password="Bungertime1")
+    if connection.is_connected():
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM dishes WHERE canMakeDish(dishName);")
+        dishes = cursor.fetchall()
+    return jsonify(dishes)
+
+@app.route("/api/create_order", methods=["POST"])
+def api_create_order():
+    res_json = request.json
+    connection = mysql.connector.connect(host="burgertime.ejrobotics.com", database="burgertime", user="jt", password="Bungertime1")
+    if connection.is_connected():
+        cursor = connection.cursor(dictionary=True)
+        vals = (res_json["orderType"], float(res_json["totalPrice"]), res_json["customerName"], res_json["specialRequests"])
+        cursor.execute("INSERT INTO customerOrder (orderTime, orderType, totalPrice, customerName, specialRequests) VALUES (NOW(), %s, %s, %s, %s);", vals)
+        connection.commit()
+        cursor.execute("SELECT MAX(orderID) AS orderID FROM customerOrder;")
+        row = cursor.fetchone()
+    return jsonify(row)
+
+@app.route("/api/clocks", methods=["GET"])
+def api_clocks():
+    connection = mysql.connector.connect(host="burgertime.ejrobotics.com", database="burgertime", user="jt", password="Bungertime1")
+    if connection.is_connected():
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute("SELECT employeeName, clockTime, clockType FROM staff NATURAL JOIN timeclock ORDER BY clockTime DESC;")
+        clocks = cursor.fetchall()
+    return jsonify(clocks)
+
+@app.route("/api/add_dish", methods=["POST"])
+def api_add_dish():
+    res_json = request.json
+    order_id = res_json["orderID"]
+    dish_id = res_json["dishID"]
+    dish_quantity = res_json["dishQuantity"]
+    connection = mysql.connector.connect(host="burgertime.ejrobotics.com", database="burgertime", user="jt", password="Bungertime1")
+    if connection.is_connected():
+        cursor = connection.cursor(dictionary=True)
+        vals = (order_id, dish_id, dish_quantity)
+        cursor.execute("INSERT INTO orderItems (orderID, dishID, dishQuantity) VALUES (%s,%s,%s);", vals)
+        connection.commit()
+    return "200"
+
+@app.route("/api/orders", methods=["GET"])
+def api_orders():
+    order_id = request.args.get("order_id")
+    connection = mysql.connector.connect(host="burgertime.ejrobotics.com", database="burgertime", user="jt", password="Bungertime1")
+    if connection.is_connected():
+        cursor = connection.cursor(dictionary=True)
+        if order_id is None:
+            cursor.execute("SELECT * FROM customerOrder ORDER BY orderID DESC;")
+            orders = cursor.fetchall()
+            return jsonify(orders)
+        else:
+            cursor.execute("SELECT dishQuantity, dishName, dishType FROM orderItems NATURAL JOIN dishes WHERE orderID=%s ORDER BY dishType;", (order_id,))
+            items = cursor.fetchall()
+            return jsonify(items)
+        
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
--- static/js/employee.js
+++ static/js/employee.js
@@ -1,12 +1,30 @@
+clocks_scroll = document.querySelector("#clocks-scroll");
 clock_in_button = document.querySelector("#clock-in");
 clock_out_button = document.querySelector("#clock-out");
 employee_id_input = document.querySelector("#employee-id-input");
+orders_scroll = document.querySelector("#orders-scroll");
+let back_button = document.querySelector("#back");
 
 let bad_flash = (elem) => {
     elem.classList.add("bad-flash");
     setTimeout(() => {
         elem.classList.remove("bad-flash");
     }, 1000);
+}
+
+let populate_order_scroll = (scroll, arr) => {
+    while (scroll.firstChild) {
+        scroll.removeChild(scroll.firstChild);
+    }
+    for (let elem of arr) {
+        let node = document.createElement("h1");
+        node.classList.add("small");
+        node.classList.add("outset");
+        node.classList.add("card");
+        let card = document.createTextNode(`[${elem.clockType.toUpperCase()}] ${elem.employeeName}, ${elem.clockTime}`);
+        node.appendChild(card);
+        scroll.appendChild(node);
+    }
 }
 
 clock_in_button.addEventListener("click", async (e) => {
@@ -23,6 +41,7 @@ clock_in_button.addEventListener("click", async (e) => {
             })
         });
         employee_id_input.value = "";
+        load_clocks();
     } else {
         bad_flash(employee_id_input);
     }
@@ -42,7 +61,59 @@ clock_out_button.addEventListener("click", async (e) => {
             })
         });
         employee_id_input.value = "";
+        load_clocks();
     } else {
         bad_flash(employee_id_input);
     }
 });
+
+let load_clocks = async () => {
+    let res = await fetch("/api/clocks");
+    let clockTimes = await res.json();
+    populate_order_scroll(clocks_scroll, clockTimes);
+}
+
+load_clocks();
+
+let orders = [];
+
+let load_orders = async () => {
+    let orders_res = await fetch("/api/orders");
+    orders = await orders_res.json();
+    for (let order of orders) {
+        let title = document.createElement("h1");
+        title.classList.add("medium");
+        title.classList.add("outset");
+        title.classList.add("card");
+        let title_text = document.createTextNode(`${order.customerName}, $${order.totalPrice}, ${order.orderTime}`);
+        title.appendChild(title_text);
+        let scroll_div = document.createElement("div");
+        scroll_div.classList.add("vertical-scroll-div");
+        scroll_div.classList.add("hidden-scrollbar");
+        scroll_div.classList.add("wrapped");
+        scroll_div.classList.add("inset");
+        scroll_div.classList.add("card");
+        title.appendChild(scroll_div);
+        let items_res = await fetch(`/api/orders?order_id=${order.orderID}`);
+        items = await items_res.json();
+        for (let item of items) {
+            console.log(item);
+            let item_title = document.createElement("h1");
+            item_title.classList.add("small");
+            item_title.classList.add("outset");
+            item_title.classList.add("card");
+            let item_title_text = document.createTextNode(`${item.dishQuantity}x ${item.dishName}`);
+            item_title.appendChild(item_title_text);
+            scroll_div.appendChild(item_title);
+        }
+        let sub_text = document.createTextNode(`${order.orderType}, ${order.specialRequests}`);
+        title.appendChild(sub_text);
+        orders_scroll.appendChild(title);
+    }
+}
+
+load_orders();
+
+back_button.addEventListener("click", (e) => {
+    window.location.href = "/";
+})
